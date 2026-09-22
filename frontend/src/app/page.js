9370ae1df87bc7dee5bb2df85973f4444185c583
26LP80883833 <26LP80883833@G304-CC0216.local>
2026-09-22 12:58:31 +0800

    fullstack3

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,45 +1,22 @@
 "use client";
 
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [bookName, setBookName] = useState("");
-  const [description, setDescription] = useState("");
+
+  const [updateID, setUpdateId] = useState(null);
+  const [editBookName, setEditBookName] = useState("");
 
   const endpoint = "http://localhost:8000/book";
 
-  const options = [
-    {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-      },
-    },
-    {
-      method: "POST",
-      headers: {
-        accept: "application/json",
-        "Content-Type": "application/json",
-      },
-
-      body: JSON.stringify({ bookName, description }),
-    },
-    (id) => ({
-      method: "DELETE",
-      headers: {
-        accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ id }),
-    }),
-  ];
-
   const getBooks = async () => {
     try {
-      const response = await fetch(endpoint, options[0]);
-      const data = await response.json();
-      setBooks(data);
+      const response = await axios.get(endpoint);
+      console.log("axios get working", response.data);
+      setBooks(response.data);
     } catch (error) {
       console.error("GET ERROR:", error);
     }
@@ -51,22 +28,39 @@ const Home = () => {
 
   const addBook = async () => {
     if (!bookName.trim()) return;
+    const book = {
+      bookName: bookName,
+    };
     try {
-      await fetch(endpoint, options[1]);
+      await axios.post(endpoint, book);
+      console.log("axios addBook working:", book);
       setBookName("");
-      setDescription("");
       getBooks();
     } catch (error) {
-      console.error("POST ERROR:", error);
+      console.error("POST ERROR", error);
     }
   };
 
   const deleteBook = async (id) => {
     try {
-      await fetch(endpoint, options[2](id));
+      await axios.delete(endpoint, { data: { id } });
+      console.log("axios delete working for ID:", id);
       getBooks();
     } catch (error) {
       console.error("DELETE ERROR:", error);
+    }
+  };
+
+  const updateBook = async (id) => {
+    if (!editBookName.trim()) return;
+    try {
+      await axios.put(endpoint, { id, bookName: editBookName });
+      console.log("axios update id", id);
+      setUpdateId(null);
+      setEditBookName("");
+      getBooks();
+    } catch (error) {
+      console.log("PUT ERROR", error);
     }
   };
 
@@ -83,18 +77,11 @@ const Home = () => {
             placeholder="Enter your book name..."
             value={bookName}
             onChange={(e) => setBookName(e.target.value)}
-            className="w-full border border-zinc-500 p-2.5 rounded-lg text-sm text-white  bg-zinc-600"
+            className="w-full border border-zinc-500 p-2.5 rounded-lg text-sm text-white bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-sky-500"
           />
-          <input
-            type="text"
-            placeholder="Enter your description..."
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="w-full border border-zinc-500 p-2.5 rounded-lg text-sm text-white  bg-zinc-600"
-          />
+
           <button
             onClick={addBook}
-            here
             className="w-full rounded-lg bg-sky-700 hover:bg-sky-800 active:scale-95 px-4 py-2.5 text-sm text-white font-semibold shadow-md shadow-sky-900/10 transition-all cursor-pointer"
           >
             Add Book
@@ -108,7 +95,7 @@ const Home = () => {
           {books?.map((bookItem) => (
             <div
               key={bookItem._id}
-              className="flex items-start justify-between bordeborder-zinc-800 bg-zinc-800/40  rounded-lg px-4 py-3 shadow-xs transition-colors"
+              className="flex items-center justify-between border-zinc-800 bg-zinc-800/40 rounded-lg px-4 py-3 shadow-xs transition-colors gap-3"
             >
               <div className="flex flex-col gap-1 pr-2">
                 <span className="text-sm font-semibold text-slate-800 dark:text-zinc-100">
@@ -120,6 +107,23 @@ const Home = () => {
                   </span>
                 )}
               </div>
+
+              <div className="flex flex-col gap-1.5">
+                <input
+                  placeholder="Enter new name"
+                  value={editBookName}
+                  type="text"
+                  onChange={(e) => setEditBookName(e.target.value)}
+                  className="px-2 py-1 border rounded-md text-xs bg-zinc-700 text-white border-zinc-600 focus:outline-none"
+                />
+                <button
+                  onClick={() => updateBook(bookItem._id)}
+                  className="w-full px-2.5 py-1 border rounded-md text-xs text-white bg-sky-600 hover:bg-sky-700 font-semibold cursor-pointer"
+                >
+                  Update
+                </button>
+              </div>
+
               <button
                 onClick={() => deleteBook(bookItem._id)}
                 className="text-xs text-red-600 hover:text-red-800 bg-red-100 hover:bg-red-200 font-semibold px-2.5 py-1 rounded transition-colors cursor-pointer shrink-0"
